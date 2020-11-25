99b3b25633ccc63800a38ad1aa68de17706a5ada
Yuri Isbener <yurisuhett@gmail.com>
2020-11-24 21:58:37 -0300

    trab feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicios_de_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            for (var num1 = 1; num1 < 10; num1++)
+            {
+                Console.WriteLine($"Tabuada do {num1}");
+                for (var num2 = 1; num2 < 11; num2++)
+                {
+                    int resultado = num1*num2;
+                    Console.WriteLine(resultado);
+                    Console.WriteLine("");
+
+                }
+            }
+            
+        }
+    }
+}
